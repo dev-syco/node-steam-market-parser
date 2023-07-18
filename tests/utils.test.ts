36e061cf21e0756583f486a69fd902352375d1b5
Pavel <pavel@itred.ru>
2023-07-18 10:02:14 +0300

    [FIX] switch httpbin to postman-echo

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -3,13 +3,13 @@ import { httpRequest } from '../src/utils';
 
 describe('Request tests', () => {
   let response: { args: { test: string } };
-  const path = '/anything';
+  const path = '/get';
   const params = {
     test: 'testParam'
   };
   before(async function() {
     this.timeout(10000)
-    response = await httpRequest({ path, json: true, hostname: 'httpbin.org', port: 443, method: 'GET', params });
+    response = await httpRequest({ path, json: true, hostname: 'postman-echo.com', port: 443, method: 'GET', params });
   });
 
   it('Request params', () => {
